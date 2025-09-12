72dbdd2339f3b187306bdb52d16c4c56c4dfe630
faithola101-sys <faithola101@gmail.com>
2025-09-12 16:14:41 +0100

    first commit

---
+++ java/Mrs Ope Job/SquareNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SquareNumber {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter number = ");
+int number = input.nextInt();
+
+ for (int i = 0, i < number, i++ ) {        
+ System.out.print() 
+ }
+}
